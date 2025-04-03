6e3df0842fc27a8f71604004fc8bcbd8dd3bc47e
MCMB16 <kmilamonsalveb@gmail.com>
2025-04-03 11:45:43 +0000

    feat: agrega modelo de receta

--- src/Receta.java
+++ src/Receta.java
@@ -1,3 +1,15 @@
 public class Receta {
+    private String nombre;
+    private String[] ingredientes;
+    private String instrucciones;
     
+    public Receta(String nombre, String[] ingredientes, String instrucciones) {
+        this.nombre = nombre;
+        this.ingredientes = ingredientes;
+        this.instrucciones = instrucciones;
+    }
+    
+    public String getNombre() { return nombre; }
+    public String[] getIngredientes() { return ingredientes; }
+    public String getInstrucciones() { return instrucciones; }
 }
